15cda1200d23522a92f379aba14b02e4a3b8fc7b
tohurtv <joshwebb84@outlook.com>
2026-03-09 21:19:19 -0600

    add default overlay and apps

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,22 +3,6 @@
 set -ouex pipefail
 
 FEDORA_VERSION="${FEDORA_VERSION:-43}"
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-#dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
 ## Install packages
 dnf5 -y install @fonts @hardware-support \
@@ -28,20 +12,12 @@ dnf5 -y install @fonts @hardware-support \
   gnome-settings-daemon \
   gnome-backgrounds \
   gnome-control-center \
-  gnome-software \
   gnome-initial-setup \
   nautilus \
-  gnome-console \
-  gnome-tweaks \
-  gnome-text-editor \
-  file-roller \
-  gnome-disk-utility \
-  gnome-system-monitor \
   NetworkManager-bluetooth \
   pipewire \
   wireplumber \
-  xdg-desktop-portal-gnome \
-  scx-manager
+  xdg-desktop-portal-gnome
 
 ## Remove packages
 dnf5 -y remove gnome-software-rpm-ostree
